a31374d40b31e2ad401573866171f3f29646a215
Yu Kadowaki <gates1de@Yus-MacBook-Pro.local>
2015-06-14 22:27:08 +0900

    create class-home-page & class-question-page

---
+++ app/controllers/lessons_controller.rb
@@ -0,0 +1,18 @@
+class LessonsController < ApplicationController
+	before_action :set_lesson, only: [:show, :questions]
+
+	def show
+		@teachers = @lesson.user.where(role: 0)
+	end
+
+	def questions
+		@questions = @lesson.questions
+		render 'questions'
+	end
+
+	private
+		def set_lesson
+			@lesson = Lesson.find(params[:id])
+		end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
+
+	resources :lessons, only: %i(show questions) do
+		collection do
+			get ':id/questions', to: 'lessons#questions', as: 'questions'
+		end
+	end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
